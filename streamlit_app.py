131fcd5cddeaa14be164e0ebf9c8bbd32488b63d
wietschecalitz <wietsche@gmail.com>
2023-05-06 18:24:08 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,17 +15,20 @@ conn = init_connection()
 #
 ## Perform query.
 ## Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=1)
-def run_query(query):
+
+with placeholder.container():
+
+    @st.cache_data(ttl=1)
+    def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT location, lat, lon, average_measure, number_of_measures FROM public.openaq_agg;")
-df = pd.DataFrame(rows, columns =['location', 'lat', 'lon', 'average_measure','number_of_measures'])
+    rows = run_query("SELECT location, lat, lon, average_measure, number_of_measures FROM public.openaq_agg;")
+    df = pd.DataFrame(rows, columns =['location', 'lat', 'lon', 'average_measure','number_of_measures'])
 
-# Print results.
-df = pd.DataFrame(rows, columns =['location', 'lat', 'lon', 'average_measure','number_of_measures'])
-st.dataframe(df)
+#    Print results.
+    df = pd.DataFrame(rows, columns =['location', 'lat', 'lon', 'average_measure','number_of_measures'])
+    st.dataframe(df)
 #for row in rows:
 #    st.write(f"{row[0]} has a :{row[1]}:")

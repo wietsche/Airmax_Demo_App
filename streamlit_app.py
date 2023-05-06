d2bdc4df45fec0155d7976df979c87c683699bf2
wietschecalitz <wietsche@gmail.com>
2023-05-06 16:21:29 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 #    st.write(f"{row[0]} has a :{row[1]}:")
 
 with st.echo(code_location='below'):
-    st.write(**st.secrets["postgres"]["host"])
+    st.write(st.secrets["postgres"]["host"])
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
@@ -58,7 +58,7 @@ with st.echo(code_location='below'):
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
 
-    st.write(**st.secrets["postgres"]["host"])
+    st.write(st.secrets["postgres"]["host"])
 
 
 
